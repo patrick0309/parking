5e004a32c1e2ea814b7c397a586707af00935ac0
Patrick Cheung <patrick.cheung@oocl.com>
2018-06-01 14:04:35 +0800

    parking 1.0

--- src/main/java/parking/AllParkingLot.java
+++ src/main/java/parking/AllParkingLot.java
@@ -4,10 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 public class AllParkingLot {
 
-		
+    public static void main(String[] args) {
+        
+    }
 
 }
